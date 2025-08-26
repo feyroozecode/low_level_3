e6c36d09b3b38c4b71c16038fbfd6741b989b019
feyroozecode <ibrahmad18@gmail.com>
2025-08-26 07:13:33 +0100

    correct implicit 99call to uindecalred function

---
+++ c/c1.c
@@ -0,0 +1,32 @@
+#include <math.h>
+#include <stdio.h>
+
+int add (int a, int b){
+    return a+b;
+}
+
+int main(){
+ int age = 25;
+ float weight = 55.7;
+ char grade = 'A';
+
+  printf("Donc vous etes agée de %d, poids %1f, VOTRE GRADE est %c\n", age, weight, grade );
+
+  // Operaters
+  int a = 14, b = 21;
+  printf("Sum: %d , Differences %d \n", (a+b), a - b);
+
+  // Loops
+  for (int i = 0; i<= sqrt(25); i++){
+      printf("Postion: %d", i);
+      printf("\n");
+  }
+  printf("\n");
+
+
+  // funciton
+  int sume_result = add(a, b);
+
+  return 0;
+
+}
