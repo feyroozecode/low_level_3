7986aca77a5de7161fff3c115ffb193edfa6d8c6
feyroozecode <ibrahmad18@gmail.com>
2025-08-28 07:46:12 +0100

    fix:calc

--- c/calc.c
+++ c/calc.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int add(int a,int b) {return a +b;}
 int sub(int x, int y){return x - y;}
@@ -18,8 +17,10 @@ int divide(int x, int y){
 // main
 int main(int argc, char *argv[]){
 
+    // Calculatrice, ADD, SUBS, MULTIPLY, DIVIDE
+    printf("******* Bienvenue sur la super Calc C-Calc *******\n");
     if(argc != 4){
-        printf("Usage: %s <numbre1>, <operator>, number2\n", argv[0]);
+        printf("Usage: %s <numbre1> <operator> number2\n", argv[0]);
         return 1;
     }
 
@@ -28,9 +29,6 @@ int main(int argc, char *argv[]){
     double num2    = atof(argv[3]);
     double result;
 
-    // Calculatrice, ADD, SUBS, MULTIPLY, DIVIDE
-    printf("************** Bienvenue sur la super Calc C-Calc *********");
-
     switch (operator) {
         case '+':
             result = add(num1, num2);
@@ -38,12 +36,12 @@ int main(int argc, char *argv[]){
         case '-':
             result = sub(num1, num2);
             break;
-        case '*':
+        case 'x':
             result = multiply(num1, num2);
             break;
         case '/':
             result = divide(num1, num2);
-
+            break;
         default:
             printf("Operateur invalide. Utiliser +, -, x, ou /. \n");
             return 1;
