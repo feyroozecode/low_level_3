362ad7ded4ad777509c9cfd5f1fd864a8ce35102
feyroozecode <ibrahmad18@gmail.com>
2025-08-29 23:51:08 +0100

    impl:: file read and write with FILE

---
+++ c/fs.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   printf("This an File R/W on CLI in c \n");
+   
+   FILE *file  = fopen("example.txt", "w"); // Ouvrir en mode Write
+   if(file == NULL){
+     printf("Erreur lors de l'ouverture du fichier .\n");
+     return -1;
+   }
+   fprintf(file, "Ceci est un exemle de fichier text.\n"); // write an text on the file 
+   fclose(file);  // close the file
+  
+   file = fopen("example.txt", "r"); // Open file in read Mode
+   if(file == NULL){
+    printf("Erreur lors de louerture du fichiers=.\n"); 
+    return -1;
+   }  
+   
+   char buffer[100];
+   while(fgets(buffer, sizeof(buffer), file) != NULL){
+      printf("Lu du fichier: %s", buffer);
+   }
+   fclose(file);
+  
+  return 0;
+}
