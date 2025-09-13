9f54eda2cc3e73679e9e3339930d92d7aefe6418
feyroozecode <ibrahmad18@gmail.com>
2025-09-13 09:58:54 +0100

    impl cpp Bank Management System 0.001

---
+++ cpp/bank_acount.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class BankAccount {
+    private:
+        std::string accountNumber;
+        double balance;
+
+   // Create the constructor]
+   public:
+     BankAccount(std::string accNum, double initialBalance): 
+             accountNumber(accNum), balance(initialBalance){
+   }
+
+    void deposit(double amount) {
+        if(amount > 0){
+            balance += amount;
+            std::cout << "Dépot de " << amount << " effectué. Nouvequ Solde : " << balance << "\n";
+        } else {
+            std::cout << "Le montant du dépoit doit etre positif";
+        }
+   }
+  void withdraw(double amount){
+    if(amount > 0 && balance >= amount) {
+      std::cout << "Retrait de " << amount << " effectué. Nouveau solde:" << balance << "\n";
+    }
+  }
+
+   double getBalance() const {
+      return balance;
+   }
+
+   std::string getAccountNumber() const {
+     return accountNumber;
+  }
+ 
+};
+
+
+int main(){
+    BankAccount myAccount("NIG202305", 1000.0);
+    std::cout<< "Compte " << myAccount.getAccountNumber() << "Créer avec un solde de " << myAccount.getBalance() << "\n";
+
+    myAccount.deposit(540);
+    myAccount.deposit(350);
+    myAccount.withdraw(200);
+
+   std::cout << "Solde final du compte" << myAccount.getAccountNumber() << " : "
+    << myAccount.getBalance() << "\n";
+
+    return 0;
+ }
